a9d6f820eefcd63bcbba5df20d8923dbb4ef5789
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-09 15:54:11 +0300

    Add extension method

---
+++ RecruitmetNotifications/RecruitmetNotifications/Extensions/ServiceExtensionCollections.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RecruitmetNotifications.Options;
+using RecruitmetNotifications.Servises;
+
+namespace RecruitmetNotifications.Extensions;
+
+public static class ServiceExtensionCollections
+{
+    public static IServiceCollection AddNotifications(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMqOptions>(configuration.GetSection(RabbitMqOptions.SectionName));
+
+        services.AddScoped<IMessageSender, MessageSender>();
+
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+            busConfigurator.UsingRabbitMq((context, rabbitMqConfigurator) =>
+            {
+                var rabbitMqOptions = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+
+                rabbitMqConfigurator.Host(rabbitMqOptions.HostName, rabbitMqOptions.VirtualHostName, configure =>
+                {
+                    configure.Username(rabbitMqOptions.UserName);
+                    configure.Password(rabbitMqOptions.Password);
+                });
+
+                rabbitMqConfigurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
