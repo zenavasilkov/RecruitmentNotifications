24ed043876b3cb6f4235c846b9dccbf6ccbf3a91
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-09 15:44:21 +0300

    Add Senders

---
+++ RecruitmetNotifications/RecruitmetNotifications/Servises/IMessageSender.cs
@@ -0,0 +1,11 @@
+﻿using RecruitmetNotifications.Messages;
+
+namespace RecruitmetNotifications.Servises;
+
+public interface IMessageSender
+{
+    Task NotifyInterviewScheduled(InterviewScheduledEvent message, CancellationToken cancellationToken);
+    Task NotifyInterviewRescheduled(InterviewRescheduledEvent message, CancellationToken cancellationToken);
+    Task NotifyInterviewCancelled(InterviewCancelledEvent message, CancellationToken cancellationToken);
+    Task NotifyCandidatePassedToInterview(CandidatePassedToIntershipEvent message, CancellationToken cancellationToken);
+}
---
+++ RecruitmetNotifications/RecruitmetNotifications/Servises/MessageSender.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using RecruitmetNotifications.Messages;
+
+namespace RecruitmetNotifications.Servises;
+
+public class MessageSender(IPublishEndpoint publishEndpoint) : IMessageSender
+{
+    public Task NotifyCandidatePassedToInterview(CandidatePassedToIntershipEvent message,
+        CancellationToken cancellationToken) => publishEndpoint.Publish(message, cancellationToken);
+
+    public Task NotifyInterviewCancelled(InterviewCancelledEvent message,
+        CancellationToken cancellationToken) => publishEndpoint.Publish(message, cancellationToken);
+
+    public Task NotifyInterviewRescheduled(InterviewRescheduledEvent message,
+        CancellationToken cancellationToken) => publishEndpoint.Publish(message, cancellationToken);
+
+    public Task NotifyInterviewScheduled(InterviewScheduledEvent message,
+        CancellationToken cancellationToken) => publishEndpoint.Publish(message, cancellationToken);
+}
