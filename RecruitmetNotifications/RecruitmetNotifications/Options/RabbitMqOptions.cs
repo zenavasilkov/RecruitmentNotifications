dd1bb298725c9ca104f5b3560afd46b693168819
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-09 14:58:56 +0300

    Add Options

---
+++ RecruitmetNotifications/RecruitmetNotifications/Options/RabbitMqOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace IMS.NotificationsCore.Options;
+
+public class RabbitMqOptions
+{
+    public const string SectionName = "RabbitMQ";
+
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string HostName { get; set; } = default!;
+    public string VirtualHostName { get; set; } = default!;
+}
